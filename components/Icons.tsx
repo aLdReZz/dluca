f138e193b678500aaedd6979c48043587ab43518
Carl <carl@example.com>
2025-12-24 15:18:29 +0800

    Add DocumentChartBarIcon for Profit and Loss page

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -300,3 +300,10 @@ export const FolderIcon: React.FC<{ className?: string }> = ({ className }) => (
               d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
     </svg>
 );
+
+export const DocumentChartBarIcon: React.FC<{ className?: string }> = ({ className }) => (
+    <svg className={className} fill="none" stroke="currentColor" viewBox="0 0 24 24">
+        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
+              d="M9 17v-2m3 2v-4m3 4v-6m2 10H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
+    </svg>
+);
